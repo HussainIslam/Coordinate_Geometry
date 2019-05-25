1ecc28607288be1f7dd5395d59e7c7585fc118ae
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2019-05-25 19:46:05 -0400

    initial commit

---
+++ src/MyPoint.java
@@ -0,0 +1,16 @@
+public class MyPoint {
+    private int x;
+    private int y;
+
+    public MyPoint(){
+        x = 0;
+        y = 0;
+    }
+
+    public MyPoint(int pointA, int pointB){
+        x = pointA;
+        y = pointB;
+    }
+
+    
+}
